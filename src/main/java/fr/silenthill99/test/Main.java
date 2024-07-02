f5b192453f09ad16c0ee179500894ed2ac280e33
BuildTools <unconfigured@null.spigotmc.org>
2024-07-02 15:10:13 +0200

    first commit

---
+++ src/main/java/fr/silenthill99/test/Main.java
@@ -0,0 +1,12 @@
+package fr.silenthill99.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Le plugin est opérationnel !");
+	}
+	
+}
